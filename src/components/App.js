906e577d2f4133b77ee56591aec84e7002a7503f
AshishDeshmukh018 <126492153+AshishDeshmukh018@users.noreply.github.com>
2023-03-14 13:41:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,20 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  function ButtonCounter() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <button onClick={handleClick}>Click me!</button>
+      <p>Button clicked {count} times.</p>
     </div>
-  )
+  );
+}
 }
 
 export default App
